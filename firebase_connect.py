43a2b414f55e5864bf9ed67b50bd60d283347bba
Vasilachi Eugen <vasilachi38@yahoo.com>
2024-11-23 00:12:23 +0200

    Refactor on firebase credentials

--- firebase_connect.py
+++ firebase_connect.py
@@ -1,11 +1,8 @@
 import requests
 import firebase_admin
 from firebase_admin import credentials, storage, firestore
-import os
-import json
 
-firebase_admin_sdk_json = os.getenv("FIREBASE_ADMIN_SDK_JSON")
-cred = credentials.Certificate(json.loads(firebase_admin_sdk_json))
+cred = credentials.Certificate("firebase-adminsdk.json")
 firebase_admin.initialize_app(cred, {"storageBucket": "rebeldot-7a26b.appspot.com"})
 
 bucket = storage.bucket()
