f705c97c7010866b2c8f2f396a3ca737df816448
Vasilachi Eugen <vasilachi38@yahoo.com>
2024-11-23 00:56:05 +0200

    Added try catch block to handle exceptions in firebase_connect.py

--- firebase_connect.py
+++ firebase_connect.py
@@ -8,9 +8,22 @@ import json
 # firebase_admin.initialize_app(cred, {"storageBucket": "rebeldot-7a26b.appspot.com"})
 
 firebase_admin_sdk_json = os.getenv("FIREBASE_ADMIN_SDK_JSON")
-if firebase_admin_sdk_json:
-    cred = credentials.Certificate(json.loads(firebase_admin_sdk_json))
-    firebase_admin.initialize_app(cred, {"storageBucket": "rebeldot-7a26b.appspot.com"})
+try:
+    if firebase_admin_sdk_json:
+        firebase_creds = json.loads(firebase_admin_sdk_json)
+        cred = credentials.Certificate(firebase_creds)
+        firebase_admin.initialize_app(
+            cred, {"storageBucket": "rebeldot-7a26b.appspot.com"}
+        )
+    else:
+        print(
+            "Error: FIREBASE_ADMIN_SDK_JSON environment variable is missing or empty."
+        )
+
+except json.JSONDecodeError:
+    print(
+        "Error: Failed to decode FIREBASE_ADMIN_SDK_JSON as JSON. Check the format in GitHub secrets."
+    )
 
 bucket = storage.bucket()
 
